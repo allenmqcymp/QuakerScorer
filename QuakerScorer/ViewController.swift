6f1b33b13d09410f187b016cd13b81cef311b3eb
Allen Ma <riallenma@gmail.com>
2019-07-01 22:36:22 -0400

    Initial Commit

---
+++ QuakerScorer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuakerScorer
+//
+//  Created by home1 on 1/07/19.
+//  Copyright © 2019 home1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

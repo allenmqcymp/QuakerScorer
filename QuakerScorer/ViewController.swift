e6b848115ed95b0006c0e1808cc63cb906814368
Mike Fu <xuanhaofu@gmail.com>
2019-07-31 15:17:26 -0400

    added camera'

--- QuakerScorer/ViewController.swift
+++ QuakerScorer/ViewController.swift
@@ -36,64 +36,7 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         imagePicker.dismiss(animated: true, completion: nil)
         mainImageView.image = info[.originalImage] as? UIImage
-    }
-//
-//    // creates a request that looks for text in rectangles
-//    func startTextDetection() {
-//        print("starting text detection")
-////        let textRequest = VNDetectTextRectanglesRequest(completionHandler: self.detectTextHandler)
-//        let textRequest = VNDetectTextRectanglesRequest(completionHandler: { (request, error) in
-//            print("helloooooo")
-//            guard let results = request.results else {
-//                print("no results :(")
-//                return
-//            }
-//        })
-//        textRequest.reportCharacterBoxes = true
-//        self.requests = [textRequest]
-//    }
-//
-//    // highlights letters
-//    func highlightLetters(box: VNRectangleObservation) {
-//        let xCord = box.topLeft.x * mainImageView.frame.size.width
-//        let yCord = (1 - box.topLeft.y) * mainImageView.frame.size.height
-//        let width = (box.topRight.x - box.bottomLeft.x) * mainImageView.frame.size.width
-//        let height = (box.topLeft.y - box.bottomLeft.y) * mainImageView.frame.size.height
-//
-//        let outline = CALayer()
-//        outline.frame = CGRect(x: xCord, y: yCord, width: width, height: height)
-//        outline.borderWidth = 1.0
-//        outline.borderColor = UIColor.blue.cgColor
-//
-//        mainImageView.layer.addSublayer(outline)
-//    }
-//
-//    func detectTextHandler(request: VNRequest, error: Error?) {
-//        print("detecting text")
-//        guard let observations = request.results else {
-//            print("no result")
-//            return
-//        }
-//
-//        let result = observations.map({$0 as? VNTextObservation})
-//
-//        DispatchQueue.main.async() {
-//            self.mainImageView.layer.sublayers?.removeSubrange(1...)
-//            for region in result {
-//                guard let rg = region else {
-//                    continue
-//                }
-//
-//                if let boxes = region?.characterBoxes {
-//                    for characterBox in boxes {
-//                        self.highlightLetters(box: characterBox)
-//                        print("highlight")
-//                    }
-//                }
-//            }
-//        }
-//    }
-    
+    }    
     
     var imagePicker: UIImagePickerController!
     
@@ -101,7 +44,7 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        session = AVCaptureSession()
+        session = AVCaptureSession()        
         session!.sessionPreset = AVCaptureSession.Preset.photo
         
         let backCamera = AVCaptureDevice.default(for: AVMediaType.video)
